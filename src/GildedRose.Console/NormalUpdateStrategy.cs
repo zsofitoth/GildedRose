9866f99c998e9e23e0150974264e14dc7b160fa5
Zsofia Toth <tothzsozso@gmail.com>
2018-07-13 23:36:09 +0200

    7/7 test cases pass for Normal Item Update

--- src/GildedRose.Console/NormalUpdateStrategy.cs
+++ src/GildedRose.Console/NormalUpdateStrategy.cs
@@ -13,15 +13,16 @@ namespace GildedRose.Console
             //end of each day (update) system lowers sell by date by 1
             item.SellIn = item.SellIn - 1;
 
-            //once the sell by date passed, quality degrades twice as fast
-            int qualityDegradeValue = item.SellIn < 0 ? 2 : 1;
-            item.Quality = item.Quality - qualityDegradeValue;
-
-
             //quality of an item is never negative
-            if (item.Quality < 0)
+            if (item.Quality <= 0)
             {
-                item.Quality = 0;
+                return;
+            }
+            else
+            {
+                //once the sell by date passed, quality degrades twice as fast
+                int qualityDegradeValue = item.SellIn < 0 ? 2 : 1;
+                item.Quality = item.Quality - qualityDegradeValue;
             }
         }
     }
