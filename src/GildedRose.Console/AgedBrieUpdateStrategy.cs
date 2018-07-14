7ee4f0b91d24fcadbc42eeb758b5e26bda256849
Zsofia Toth <tothzsozso@gmail.com>
2018-07-14 10:07:39 +0200

    aged brie refactoring so it aligns with legacy code

--- src/GildedRose.Console/AgedBrieUpdateStrategy.cs
+++ src/GildedRose.Console/AgedBrieUpdateStrategy.cs
@@ -12,7 +12,13 @@ namespace GildedRose.Console
         {
             item.SellIn = item.SellIn - 1;
 
-            if (item.Quality < 50)
+            // this is necessary to get the same results for negative sellin and negative item quality as in the legacy code
+            // as invalid upper and lower bounds are not handled there 
+            if (item.Quality < 0 && item.SellIn < 0)
+            {
+                item.Quality = item.Quality + 2;
+            }
+            else if (item.Quality < 50)
             {
                 item.Quality = item.Quality + 1;
             }
