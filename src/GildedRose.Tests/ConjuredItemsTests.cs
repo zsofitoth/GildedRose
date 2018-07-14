848b0d741614f5929c927fdb0082b21af8c2eda2
Zsofia Toth <tothzsozso@gmail.com>
2018-07-14 09:32:49 +0200

    conjured items test cases were added and are failing

---
+++ src/GildedRose.Tests/ConjuredItemsTests.cs
@@ -0,0 +1,61 @@
+﻿using GildedRose.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class ConjuredItemsTests
+    {
+        [Fact]
+        public void TestConjuredItem_PositiveSellIn()
+        {
+            var conjuredItem = new Item
+            {
+                Name = "Conjured Mana Cake",
+                SellIn = 3,
+                Quality = 6
+            };
+
+            Helper.SetUpItem(conjuredItem);
+
+            Assert.Equal(2, conjuredItem.SellIn);
+            Assert.Equal(4, conjuredItem.Quality);
+        }
+
+        [Fact]
+        public void TestConjuredItem_NegativeSellIn()
+        {
+            var conjuredItem = new Item
+            {
+                Name = "Conjured Something Else",
+                SellIn = -1,
+                Quality = 6
+            };
+
+            Helper.SetUpItem(conjuredItem);
+
+            Assert.Equal(-2, conjuredItem.Quality);
+            Assert.Equal(2, conjuredItem.Quality);
+        }
+
+        [Fact]
+        public void TestConjuredItem_InvalidQuality()
+        {
+            var conjuredItem = new Item
+            {
+                Name = "Conjured Mana Cake",
+                SellIn = -1,
+                Quality = -1
+            };
+
+            Helper.SetUpItem(conjuredItem);
+
+            Assert.Equal(-2, conjuredItem.SellIn);
+            Assert.Equal(0, conjuredItem.Quality);
+        }
+    }
+}
